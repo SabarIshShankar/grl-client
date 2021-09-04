1b8bc1061dc3f3cb953b0086e8015dedebe2980d
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-04 14:16:53 +0530

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
-  uri: 'https://grl.sabarishshankar.repl.co',
+  uri: 'https://xuqqb.sse.codesandbox.io/',
 });
 
 // kind of a middleware to add authorization headers to API calls
